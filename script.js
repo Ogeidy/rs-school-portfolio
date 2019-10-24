f8498fe4fd53af5dfbe216bb45c05a655aacf203
Yakov Netbook <yakov-mamontov@yandex.ru>
2019-10-24 22:38:15 +0300

    feat: add animation

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let items = document.querySelectorAll('.item');
 let currentItem = 0;
+let isEnable = true;
+
+function cnangeCurrentItem() {
+    currentItem = (currentItem - 1 + items.length) % items.length;
+}
+
+function hideItem(direction) {
+    isEnable = false;
+    items[currentItem].classList.add(direction);
+    items[currentItem].addEventListener('animationend', function() {
+        this.classList.remove(direction);
+        this.classList.remove('active');
+        isEnable = true;
+    });
+}
 
 document.querySelector('.control.left').addEventListener('click', function() {
-    console.log('Left');
-    items[currentItem].classList.remove('active');
-    currentItem = (currentItem - 1 + items.length) % items.length;
-    console.log(currentItem);
-    items[currentItem].classList.add('active');
+    if (isEnable) {
+        hideItem('to_left');
+        cnangeCurrentItem();
+        items[currentItem].classList.add('active');
+    }
 });
 
 document.querySelector('.control.right').addEventListener('click', function() {
-    console.log('Right');
-    items[currentItem].classList.remove('active');
-    currentItem = (currentItem + 1 + items.length) % items.length;
-    console.log(currentItem);
-    items[currentItem].classList.add('active');
+    if (isEnable) {
+        hideItem('to_right');
+        cnangeCurrentItem();
+        items[currentItem].classList.add('active');
+    }
 });
